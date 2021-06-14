c0f3435bd33ed32ecd38fdef483c86aadb919a8e
Reo Ando <ando.reo420@gmail.com>
2021-06-14 17:35:38 +0900

    Dagger Hiltを導入する #3

--- app/src/main/java/com/example/androidapptemplate/MainActivity.kt
+++ app/src/main/java/com/example/androidapptemplate/MainActivity.kt
@@ -2,7 +2,10 @@ package com.example.androidapptemplate
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dagger.hilt.EntryPoint
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/com/example/androidapptemplate/MyApp.java
@@ -0,0 +1,9 @@
+package com.example.androidapptemplate;
+
+import android.app.Application;
+
+import dagger.hilt.android.HiltAndroidApp;
+
+@HiltAndroidApp
+public class MyApp extends Application {
+}
